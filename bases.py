e0a35e3a47e582b3e1b64047ece14890c4639f60
Pierre Laclau <pielaclau@gmail.com>
2018-05-09 09:35:50 +0200

    Corrected indentation

--- bases.py
+++ bases.py
@@ -2,87 +2,80 @@ class ScreenStates():
     INACTIVE = 0
     ACTIVE   = 1
 
-class Component(Object):
-	def __init__(self, size):
-		self.size = size
-		self.children = []
+class Component(object):
+    def __init__(self, size):
+        self.size = size
+        self.children = []
 
-	def add(self, child):
-		self.children.append(child)
+    def add(self, child):
+        self.children.append(child)
 
-	def update(self, window, events):
-		result = False
+    def update(self, window, events):
+        result = False
 
-		for child in self.children:
-			result = child.update(window, events)
+        for child in self.children:
+            result = child.update(window, events)
 
-			if result:
-				return result
+            if result:
+                return result
 
-		return False
+        return False
 
 class Screen(Component):
     '''
-		Called when the screen gain focus
+        Called when the screen gain focus
     '''
     def focus(self):
-    	pass
+        pass
 
     '''
-		Called when the screen loose focus
+        Called when the screen loose focus
     '''
     def unfocus(self):
-    	pass
+        pass
 
 class Button(Component):
-	'''
-		Create button with images for specific states
-	'''
-	def __init__(self, image, position, size):
-		super(Button, self).__init__(size)
+    '''
+        Create button with images for specific states
+    '''
+    def __init__(self, image, position, size):
+        super(Button, self).__init__(size)
 
-		self.image = pygame.transform.scale(image, size)
-		self.position = position
+        self.image = pygame.transform.scale(image, size)
+        self.position = position
 
-		# Called when
-		self.callback = callback
-
-	def update(self, window, events):
-		if (kejrlkjqf)
-			rteurn instance
-
-		window.blit(self.image, self.position)
-
+        # Called when
+        self.callback = callback
 
 
 class EventButton(Button):
-	def __init__(self, image, position, size, callback):
-		super(EventButton, self).__init__(image, position, size)
+    def __init__(self, image, position, size, callback):
+        super(EventButton, self).__init__(image, position, size)
 
-		# Called when clicked
-		self.callback = callback
+        # Called when clicked
+        self.callback = callback
 
-	def update(self, window, events):
-		# TODO manage click
+    def update(self, window, events):
+        # TODO manage click
 
-		window.blit(self.image, self.position)
+        window.blit(self.image, self.position)
 
-		return False
+        return False
 
 
 
 class ScreenChangeButton(Button):
-	def __init__(self, image, position, size, screen=True):
-		super(EventButton, self).__init__(image, position, size)
+    def __init__(self, image, position, size, screen=True):
+        super(EventButton, self).__init__(image, position, size)
 
-		# Screen displayed on click
-		self.screen = screen
+        # Screen displayed on click
+        self.screen = screen
 
-	def update(self, window, events):
-		# TODO manage click
-		if False:
-			return self.screen
+    def update(self, window, events):
+        # TODO manage click
+        if False:
+            return self.screen
 
-		window.blit(self.image, self.position)
+        window.blit(self.image, self.position)
 
-		return False
+        return False
--- screens/pokemoskito.py
+++ screens/pokemoskito.py
@@ -1,22 +1,22 @@
 from ..bases import Screen
 
 class Pokemoskito(Screen):
-	def __init__(self):
-		super(Pokemoskito, self).__init__()
+    def __init__(self):
+        super(Pokemoskito, self).__init__()
 
-	
-	def focus(self):
-		# TODO generate random monster
-		pass
+    
+    def focus(self):
+        # TODO generate random monster
+        pass
 
-	def update(self, window, events):
-		pass
+    def update(self, window, events):
+        pass
 
 
 class Entity(object):
-	def __init__(self):
-		# Life in percent
-		self.life = 100
+    def __init__(self):
+        # Life in percent
+        self.life = 100
 
 class Player(Entity):
-	def __init__(self):
+    def __init__(self):
