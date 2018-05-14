eab2113fd04ea45f26fc415ffa411e7b5ec2a267
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2018-05-14 18:58:19 +0800

    Messages for pokemoskito and actions

--- screens/pokemoskito.py
+++ screens/pokemoskito.py
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-from ..bases import Screen
+from bases import Screen
 from ..util import Grid
-
+from collections import deque
 import pygame
 
-=======
-from bases import Screen
->>>>>>> master
 
 class Pokemoskito(Screen):
 	def __init__(self, size):
@@ -14,8 +10,14 @@ class Pokemoskito(Screen):
 
 		self.grid = Grid(size, 3, 3)
 		self.buttongrid = Grid(size(self.grid), 3, 2, 20)
-
-		self.actions = []
+		self.messages = Messages()
+		
+		self.actions = [
+			Action("Spit", 20, {"armor": -5}),
+			Action("Intercourse break", -10, {"armor": 5}, 0),
+			Action("Space Dog", 25),
+			Action("AFK", 0, {"armor": 30})
+		]
 		self.turn = 0
 		
 		#self.ui = pygame.image.load("assets/pokemoskito/ui.png")
@@ -28,10 +30,15 @@ class Pokemoskito(Screen):
 						((50*x+100,50*y+100,150),),
 						self.grid.pos(1, 3, self.buttongrid.pos(x, y)),
 						self.buttongrid.size(),
-						lambda: print("oui")
+						self.createLambda(actions[x + 2 * y])
 					)
 				)
-
+				
+		self.add(self.messages)
+		
+	def createLambda(self, action):
+		return lambda: if self.messages.disabled: action.apply(self.player, self.enemy)
+	
 	def focus(self, player):
 		self.player = player
 		self.enemy = Enemy()
@@ -45,7 +52,7 @@ class Pokemoskito(Screen):
 		#window.text(entity.name, (x, y))
 		#window.rect((x, y + size * 4 / 6), (size * entity.life / 100, size / 6))
 
-	def update(self, window, events):
+	def update(self, window, events):		
 		# Affichage de l'ui de base
 		#window.blit(self.ui, (0, 0))
 
@@ -59,7 +66,65 @@ class Pokemoskito(Screen):
 
 		# Et des enfants
 		super(Pokemoskito, self).update(window, events)
+	
 
+class Messages(Paragraph):
+	def __init__(self, size, position, font):
+		self.messages = deque([])
+		
+		self.current = ""
+		self.index = 0
+		self.disabled = True
+
+		super(Messages, self).__init__(size, position, font)
+		
+	def add(self, message):
+		self.messages.appendleft(message)
+		self.disabled = False
+	
+	def next(self):
+		self.index = 0
+		
+		if len(self.messages) == 0:
+			self.disabled = True
+			return ""
+			
+		return self.messages.pop()
+	
+	def update(self, window, events):
+		# touche entree
+		if False and self.index == len(self.current):
+			self.current = next()
+			
+		if not self.disabled:
+			if self.index < len(self.current):
+				# more delay?
+				index += 1
+
+				self.set_text(self.current[:index])
+				
+			super(Messages, self).update(window, events)
+
+class Action(object):
+	def __init__(self, name, damage=0, buff={}, target=1):
+		self.name = name
+		self.damage = damage
+		self.buffs = buffs
+		self.target = target
+		
+	def apply(self, source, enemy, messages):
+		target = self.target == 0 ? source : enemy;
+		
+		messages.add(source.name + " uses " + self.name)
+		
+		# Apply damage
+		if self.damage:
+			target.damage(self.damage, messages)
+		
+		# Then (de)buffs
+		if self.buffs:
+			for name in self.buffs:
+				target.stats[name] = self.buffs[name]
 
 class Entity(object):
 	def __init__(self, name, texture, size):
@@ -67,7 +132,36 @@ class Entity(object):
 		self.life = 100
 		self.name = name
 		self.texture = pygame.transform.resize(texture, size)
+		
+		# Detailed stats
+		self.stats = {
+			armor: 10,
+			poison: 0
+		}
+		
+	def turnover(self):
+		if self.stats.poison:
+			self.life -= self.stats.poison
 
+	def damage(self, damages, messages):
+		# Heal
+		if damages < 0:
+			# We do not care about having more than 100% life :3
+			self.life -= damages
+			messages.add(self.name + " recovered " + damages + " HP")
+		else:
+			# With enough armor, damages heals
+			dmg = damages * (50 - self.stats.armor) / 100
+			
+			self.life -= dmg
+			messages.add(self.name + " lost " + dmg + " HP")
+	
+	def add(self, stat, value):
+		if self.stats[stat]:
+			self.stats[stat] += value
+		else:
+			self.stats[stat] = value
+			
 class Enemy(Entity):
 	def __init__(self, name, size):
 		# TODO load random texture
--- util.py
+++ util.py
@@ -21,3 +21,49 @@ class Grid(object):
 
 		return (width * self.size + (width - 1) * self.padding, \
 			height * self.size + (height - 1) * self.padding)
+
+class Paragraph(Component):
+	def __init__(self, size, position, font):
+		super(Paragraph, self).__init__(self, size)
+
+		self.last_size = 0
+		self.position = position
+		self.font = font
+
+	def set_text(self, text):
+		if self.content != text
+			self.content = text
+
+			self.render()
+
+	def render(self):
+		self.last_size = self.size[0]
+
+		parts = self.content.split(" ")
+		lines = []
+		line = ""
+
+		for part in parts:
+			if self.font.size(line + part) < self.size[0]:
+				line += part
+			else:
+				lines.append(line)
+				line = part
+
+		lines.append(part)
+
+		self.surface = pygame.Surface((self.size[0], self.size[1]))
+		
+		height = self.font.get_linesize()
+		y = 0
+
+		for line in lines:
+			self.surface.blit(self.font.render(line, False), (0, y))
+			y += height
+
+	def update(self, window, events):
+		if self.size[0] != self.last_size:
+			self.render()
+
+		window.blit(self.surface, self.position)
+
